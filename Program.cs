76dc659f8224b86ad4f2a78bc81ef8a35d7a5874
user <user@WINDOWS-5AN5BRH>
2024-12-02 20:15:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<int, int[]> generateFibonacci = count =>
+        {
+            if (count <= 0) return new int[0];
+            if (count == 1) return new int[] { 0 };
+            if (count == 2) return new int[] { 0, 1 };
+
+            int[] fibArray = new int[count];
+            fibArray[0] = 0;
+            fibArray[1] = 1;
+
+            for (int i = 2; i < count; i++)
+            {
+                fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+            }
+
+            return fibArray;
+        };
+
+        Console.WriteLine("Введіть кількість чисел Фібоначчі:");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        int[] fibonacciNumbers = generateFibonacci(count);
+        Console.WriteLine($"Числа Фібоначчі: {string.Join(", ", fibonacciNumbers)}");
+    }
+}
